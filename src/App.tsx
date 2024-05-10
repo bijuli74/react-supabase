978b6392354a0ecd8d19e79a910aaff56afe9f79
bijuli74 <rust_bondcr007@yahoo.com>
2024-05-10 18:56:14 +0530

    added skeleton router components

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import './App.css'
+import { RouterProvider, createBrowserRouter, Outlet } from 'react-router-dom'
+import NavBar from './NavBar'
+import AllPosts from './AllPosts';
+import PostView from './PostView';
+import MessageBoard from './MesssageBoard';
+import Welcome from './Welcome';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <MessageBoard />,
+        children: [
+          {
+            path: ":pageNumber",
+            element: <AllPosts />,
+          },
+          {
+            path: "post/:postid",
+            element: <PostView />,
+          },
+        ],
+      },
+
+      {
+        path: "welcome",
+        element: <Welcome />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
+
+function Layout() {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+}
